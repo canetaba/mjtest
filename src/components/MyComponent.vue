ab969c40a4069a5bf6dcf9e5995de2e862827886
mjbriceno <mjbriceno@subtel.gob.cl>
2024-01-25 09:12:00 -0300

    Desactivar el selector

--- src/components/MyComponent.vue
+++ src/components/MyComponent.vue
@@ -12,13 +12,53 @@
         label="Escoja el firmante"
     ></v-select>
   </div>
+
+
+  <div>
+    <v-app>
+      <v-container>
+        <v-select
+            :items="items"
+            v-model="itemsSelected"
+            item-title="title"
+            item-value="id"
+            multiple
+        >
+          <template v-slot:item="{ props, item }">
+            <v-list-item
+                v-bind="props"
+                :subtitle="item.raw.department"
+                :disabled="item.raw.disabled"
+            ></v-list-item>
+          </template>
+        </v-select>
+      </v-container>
+    </v-app>
+  </div>
 </template>
 
 <script>
 export default {
   name:  'MyComponente',
-  data: () => ({
-    items: ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4'],
+  data: () => (
+      {
+    items: [ {
+     title:  'Opción 1',
+      id: 442,
+     disabled: false,
+    }, {
+      title:  'Opción 2',
+      id: 443,
+      disabled: true,
+    },
+      {
+        title:  'Opción 3',
+        id: 444,
+        disabled: false,
+      }
+    ],
+
+
   }),
 }
 </script>
