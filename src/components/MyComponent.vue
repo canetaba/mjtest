bbbd9d680c6b7332616ccf7cc2e7d292ca451950
María José Briceño Nikulin <canetaba@users.noreply.github.com>
2024-01-24 23:20:57 -0300

    Selector
    Prueba de selección en vuejs

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <v-select
+        :items="items"
+        density="comfortable"
+        label="Escoja el visador"
+    ></v-select>
+
+    <v-select
+        :items="items"
+        density="comfortable"
+        label="Escoja el firmante"
+    ></v-select>
+  </div>
+</template>
+
+<script>
+export default {
+  name:  'MyComponente',
+  data: () => ({
+    items: ['Opción 1', 'Opción 2', 'Opción 3', 'Opción 4'],
+  }),
+}
+</script>
